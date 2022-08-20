c10367b35a5ab1d513630234aea5608a099297d3
adrytech <adry.delucchi2005@gmail.com>
2022-08-20 16:09:58 +0200

    php progress

--- php/login.php
+++ php/login.php
@@ -1,5 +1,15 @@
 <?php
 
-    
+include 'database.php';
+$mysqli = new mysqli("localhost", "root", "", "db_derthona");
 
+ $name = $_POST['name'];
+ $surname = $_POST['surname'];
+ $password = $_POST['password'];
+
+ echo $name ."  ". $surname;
+
+ $mysqli->query("SELECT id, FROM users");
+
+ echo $mysqli;
 ?>
