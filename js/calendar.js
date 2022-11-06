7f6da4939e3148b3fd160140e05d01c68dd746b5
adrytech <adry.delucchi2005@gmail.com>
2022-11-06 16:00:53 +0100

    Co-authored-by: Alessandro De Iturbe <alessandroDeIturbe@users.noreply.github.com>

---
+++ js/calendar.js
@@ -0,0 +1,8 @@
+const date = new Date();
+// const [month, day, year] = [oggi.getMonth, oggi.getDate, oggi.getFullYear];
+const today = date.getDay();
+if (today == 2 || today == 4) {
+    console.log("muovi il culo e vai a veder tua sorella");
+} else {
+    console.log('Muori')
+}
--- php/login.php
+++ php/login.php
@@ -6,11 +6,17 @@ include 'database.php';
  $name = strtolower($_POST['name']);
  $surname = strtolower($_POST['surname']);
  $password = $_POST['password'];
+ $url = '../reservedarea.html';
 
  $conn = new mysqli('127.0.0.1', 'root', '', 'db_derthona');
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+}
+
+function redirect($url) {
+  header('Location: '.$url);
+  die();
 }
 
 $sql = "SELECT id, name, surname, password FROM users";
@@ -22,9 +28,11 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
 
     if($name  === strtolower($row["name"]) && $surname === strtolower($row["surname"]) && $password === $row["password"]){
-        echo "ciao " .$row["name"];
+        // echo "ciao " .$row["name"];
         
-    }else{
+      redirect($url);
+        
+    } else {
     }
 
   }
